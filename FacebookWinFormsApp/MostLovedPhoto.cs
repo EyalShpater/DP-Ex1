c66ed500f784c670c0b2b948e1b59b89f71323c9
tairdahari <tairda@mta.ac.il>
2024-01-19 16:17:38 +0200

    added PerfectMatch and MostLikedPhoto in album

---
+++ FacebookWinFormsApp/MostLovedPhoto.cs
@@ -0,0 +1,39 @@
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFacebookFeatures
+{
+    internal class MostLovedPhoto
+    {
+        internal static void DisplayMostLovedPhoto(Album io_Album)
+        {
+            if (io_Album == null)
+            {
+                throw new ArgumentNullException(nameof(io_Album));
+            }
+
+            Photo mostLovedPhoto = findMostLovedPhoto(io_Album);
+
+            if (mostLovedPhoto != null)
+            {
+                Console.WriteLine($"Most Loved Photo in {io_Album.Name}: {mostLovedPhoto.PictureNormalURL}");
+            }
+            else
+            {
+                Console.WriteLine($"No photos found in {io_Album.Name}");
+            }
+        }
+
+        private static Photo findMostLovedPhoto(Album io_Album)
+        {
+            return io_Album.Photos
+                .Where(photo => photo.LikedBy.Count > 0)
+                .OrderByDescending(photo => photo.LikedBy.Count)
+                .FirstOrDefault();
+        }
+    }
+}
