0a05e12acc78a96785bc0e76f7c46334d349d4c2
shpater1234 <shpaters@hotmail.com>
2024-01-21 11:28:27 +0200

    try

--- FacebookWinFormsApp/MainForm.Designer.cs
+++ FacebookWinFormsApp/MainForm.Designer.cs
@@ -238,8 +238,8 @@
             // 
             // buttonDownloadAlbum
             // 
-            this.buttonDownloadAlbum.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.buttonDownloadAlbum.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonDownloadAlbum.AutoSize = true;
             this.buttonDownloadAlbum.BackgroundImage = global::BasicFacebookFeatures.Properties.Resources.download;
